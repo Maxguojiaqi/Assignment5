844b1b4984fd8f17bf03bd769080105e90e8ef37
Max <maxguojiaqi@gmail.com>
2020-03-09 20:40:22 -0400

    finish up the code, add comment

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 
-
+// Create the time object and update it every second
 $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
 setInterval(function() {
     $('#currentDay').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-}, 1000); //Every minute
-
+}, 1000); //Update every second
 
 
 for (let i =9; i < 18; i++)
 {   
     // create the DOM div for the entire row 
     let scheduleRowDiv = $('<div>');
-    scheduleRowDiv.addClass("row border border-top-0 border-secondary text-center");
+    scheduleRowDiv.addClass("row border border-top-0 border-info text-center");
     scheduleRowDiv.attr('id', 'calendar-row'+i);
     // create the DOM div displays the time
     let scheduleColDiv1 = $('<div>');
@@ -30,7 +29,7 @@ for (let i =9; i < 18; i++)
     }
     // create the DOM div for the input text area
     let scheduleColDiv2 = $('<textarea>');
-    scheduleColDiv2.addClass('col-10 align-middle');
+    scheduleColDiv2.addClass('col-10 align-middle text-dark font-italic');
     scheduleColDiv2.attr('placeholder', 'Enter your plan here...');
 
     let currentHour = moment().format('h a').substring(0,2);
@@ -40,37 +39,40 @@ for (let i =9; i < 18; i++)
     {
         currentHour= parseInt(currentHour)+12;
     }
-
+    // depend on the hour relative to the current time, assign different background color.
     if ( i < parseInt(currentHour))
     {
-        scheduleColDiv2.addClass("bg-secondary");
+        scheduleColDiv2.addClass("past");
     }
     else if(i == parseInt(currentHour))
     {
-        scheduleColDiv2.addClass("bg-danger");
+        scheduleColDiv2.addClass("present");
     }
     else
     {
-        scheduleColDiv2.addClass("bg-success");
+        scheduleColDiv2.addClass("future");
     }
 
-    let itemVal = localStorage.getItem('todo'+i);
-    scheduleColDiv2.text(itemVal);
-    
+    // read out information stored in local storage
+    let itemVal = localStorage.getItem('todo' + i);
+
+    if( itemVal!=null)
+    {
+        scheduleColDiv2.text(itemVal);
+    }
     // create the DOM div for the store button
     let scheduleColDiv3 = $('<button>');
-    scheduleColDiv3.addClass('col-1 btn btn-info btn-lg btn-block fas fa-lock');
+    scheduleColDiv3.addClass('col-1 saveBtn fas fa-lock');
     scheduleColDiv3.on('click', function()
     {
         localStorage.setItem('todo'+i, scheduleColDiv2.val());
     });
 
-
     // add all the coloum to the row
     scheduleRowDiv.append(scheduleColDiv1);
     scheduleRowDiv.append(scheduleColDiv2);
     scheduleRowDiv.append(scheduleColDiv3);
-
+    // add row to the schedule content
     $('.scheduleContent').append(scheduleRowDiv);
 
 }
